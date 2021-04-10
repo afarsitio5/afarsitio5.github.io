f1a9e7093c3638360447715311f9a5246899b7ee
afarsitio5 <afarsitio5@gmail.com>
2021-04-10 04:54:20 -0500

    prueba3

--- js/init.js
+++ js/init.js
@@ -11,9 +11,10 @@ firebase.initializeApp({
   */
   apiKey: "AIzaSyC9Gvm852NtPQbrf3RFJ0N2Yib6REMG8T0",
     authDomain: "afarsitio5-c617a.firebaseapp.com",
+    databaseURL: "https://afarsitio5.firebaseio.com",
     projectId: "afarsitio5-c617a",
     storageBucket: "afarsitio5-c617a.appspot.com",
     messagingSenderId: "267228304883",
     appId: "1:267228304883:web:71c69cb0920f37cc55ee86",
-    measurementId: "G-Z8GQP7CLHM"
+    //measurementId: "G-Z8GQP7CLHM"
 });
