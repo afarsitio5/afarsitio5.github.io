223e27528a8c164c679b036bd22ad46a4b30c8f6
afarsitio5 <afarsitio5@gmail.com>
2021-04-10 05:39:20 -0500

    cambio id del proyecto

--- js/init.js
+++ js/init.js
@@ -1,17 +1,8 @@
 // @ts-ignore
 firebase.initializeApp({
-  /*
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
-  */
-  apiKey: "AIzaSyC9Gvm852NtPQbrf3RFJ0N2Yib6REMG8T0",
+    apiKey: "AIzaSyC9Gvm852NtPQbrf3RFJ0N2Yib6REMG8T0",
     authDomain: "afarsitio5-c617a.firebaseapp.com",
-    databaseURL: "https://afarsitio5.firebaseio.com",
+    //databaseURL: "https://afarsitio5.firebaseio.com",
     projectId: "afarsitio5-c617a",
     storageBucket: "afarsitio5-c617a.appspot.com",
     messagingSenderId: "267228304883",
