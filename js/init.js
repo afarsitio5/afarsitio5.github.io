1847e6da0bbb11d6ab0760c2b5cb22979e0ce9d9
afarsitio5 <82254754+afarsitio5@users.noreply.github.com>
2021-04-10 04:12:31 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 // @ts-ignore
 firebase.initializeApp({
+  /*
   apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
   authDomain: "gilpgiotx.firebaseapp.com",
   databaseURL: "https://gilpgiotx.firebaseio.com",
@@ -7,4 +8,12 @@ firebase.initializeApp({
   storageBucket: "gilpgiotx.appspot.com",
   messagingSenderId: "362186415969",
   appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  */
+  apiKey: "AIzaSyC9Gvm852NtPQbrf3RFJ0N2Yib6REMG8T0",
+    authDomain: "afarsitio5-c617a.firebaseapp.com",
+    projectId: "afarsitio5-c617a",
+    storageBucket: "afarsitio5-c617a.appspot.com",
+    messagingSenderId: "267228304883",
+    appId: "1:267228304883:web:71c69cb0920f37cc55ee86",
+    measurementId: "G-Z8GQP7CLHM"
 });
