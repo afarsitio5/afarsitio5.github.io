cdeef2dca3de35438e7e8d2679addc7cfe5e508f
afarsitio5 <afarsitio5@gmail.com>
2021-04-12 12:08:52 -0500

    asdfg

--- js/config.js
+++ js/config.js
@@ -10,34 +10,6 @@ class MiNav extends HTMLElement {
           home
         </span>
         Inicio
-      </a>
-      <a href="formulario.html">
-        <span
-          class="material-icons">
-          fact_check
-        </span>
-        Formulario
-      </a>
-      <a href="gps.html">
-        <span
-          class="material-icons">
-          location_searching
-        </span>
-        GPS
-      </a>
-      <a href="archivos.html">
-        <span
-          class="material-icons">
-          attach_file
-        </span>
-        Archivos
-      </a>
-      <a href="ayuda.html">
-        <span
-          class="material-icons">
-          help
-        </span>
-        Ayuda
       </a>`;
   }
 }
